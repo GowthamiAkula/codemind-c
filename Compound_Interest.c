10fbbbe3598e24b609668d0eadb0d7f21803b57c
GowthamiAkula <147791859+GowthamiAkula@users.noreply.github.com>
2023-10-20 11:41:48 +0530

    #include<stdio.h> #include<math.h> int main() {     int p,r,t;     scanf("%d%d%d",&p,&r,&t);     float R=r/100.0;     float ci=(p*pow(1+R,t))-p;     printf("%.2f",ci);     return 0; }

---
+++ Compound_Interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int p,r,t;
+    scanf("%d%d%d",&p,&r,&t);
+    float R=r/100.0;
+    float ci=(p*pow(1+R,t))-p;
+    printf("%.2f",ci);
+    return 0;
+}
